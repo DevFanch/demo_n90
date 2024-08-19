4a873d1f089953a4fb34539bf8a5dc39a9f7bb06
devfanch <devfanch44@gmail.com>
2024-08-19 17:02:03 +0200

    Démo  Module 1-3

---
+++ src/Controller/TestController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(): Response
+    {
+        $name = 'toto';
+        $age = 40;
+        $hobbies = ['chill', 'plage', 'dev'];
+        $now = new \DateTime();
+
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+            'name' => $name,
+            'age' => $age,
+            'hobbies' => $hobbies,
+            'now' => $now
+        ]);
+    }
+    
+    #[Route('/bidulle', name: 'app_bidulle')]
+    public function truc(): Response
+    {
+        return $this->render('test/bidulle.html.twig', [
+            'method' => 'Truc from Bidulle route'
+        ]);
+    }
+}
