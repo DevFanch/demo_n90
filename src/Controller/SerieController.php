a063ddeba2e5f8f74da387e7d9c1cb901bb4f09d
devfanch <devfanch44@gmail.com>
2024-08-20 16:19:22 +0200

    M05 D01-02

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,8 +14,14 @@ class SerieController extends AbstractController
     #[Route('/', name: 'list')]
     public function index(): Response
     {
+        // Gen movies list in array
+        $movies = [
+            'The Matrix', 'Inception', 'Interstellar', 'The Prestige'
+        ];
+
         return $this->render('serie/list.html.twig', [
             'title' => 'List Films',
+            'movies' => $movies
         ]);
     }
     #[Route('/create', name: 'create')]
@@ -24,10 +32,43 @@ class SerieController extends AbstractController
         ]);
     }
     #[Route('/{id}', name: 'detail', requirements: ['id' => '\d+'])]
-    public function detail(): Response
+    public function detail(Serie $serie, EntityManagerInterface $em): Response
     {
+        // create serie
+//        $serie = new Serie();
+//        $serie
+//            ->setName('Serie ' . $id)
+//            ->setBackdrop('Serie ' . $id)
+//            ->setPoster('Serie ' . $id)
+//            ->setDateCreated(new \DateTime())
+//            ->setDateModified(new \DateTime())
+//            ->setFirstAirDate(new \DateTime('-1 year'))
+//            ->setLastAirDate(new \DateTime('-6 month'))
+//            ->setGenres('Serie ' . $id)
+//            ->setOverview('Serie ' . $id)
+//            ->setPopularity(120.20)
+//            ->setVote(5.30)
+//            ->setStatus('Cancelled')
+//            ->setTmdbId(987354);
+//
+//        dump($serie);
+//        // Persist
+//        $em->persist($serie);
+//        $em->flush();
+//
+//        dump($serie);
+//
+//        // Edit serie
+//        $serie->setName('Ma nouvelle série');
+//        // Update
+//        $em->flush();
+//
+//        dump($serie);
+
+
         return $this->render('serie/detail.html.twig', [
             'title' => 'Detail',
+            'serie' => $serie
         ]);
     }
 }
